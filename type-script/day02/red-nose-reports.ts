0fafc22ad523f3aa86b59590e033f8c4046d7bc9
Hristijan Vilos <hristijan.vilos@protonmail.com>
2024-12-02 20:04:30 +0100

    Add day02

--- type-script/day02/red-nose-reports.ts
+++ type-script/day02/red-nose-reports.ts
@@ -1,37 +1,43 @@
 import * as fs from "fs";
 
-const f: string = fs.readFileSync("../inputs/day02/test_input.txt", { encoding: "utf-8" });
+const f: string = fs.readFileSync("../inputs/day02/input.txt", { encoding: "utf-8" });
 const reports: number[][] = f.trim().split("\n").map(x => x.trim().split(/\s+/).map(y => Number(y)));
 
-function part1(reports: number[][]): number {
+function solution(reports: number[][], tolerance: number): number {
     let result: number = 0;
     for (const report of reports) {
-        let safe: boolean = true;
-        const increaseOrder: boolean = (report[0] - report[1]) < 0;
-        for (let i = 0; i < report.length - 1; i++) {
-            if (increaseOrder !== (report[i] - report[i + 1]) < 0) {
-                safe = false;
-                break;
-            }
-            const diff: number = Math.abs(report[i] - report[i + 1]);
-            if (diff < 1 || diff > 3) {
-                safe = false;
-                break
-            }
-        }
-        if (safe) {
-            result += 1;
-        }
+        result += safeOrUnsafe(report, tolerance);
     }
     return result;
 }
 
-function part2(reports: number[][]): number {
-    // > 264  307< 
-    // [ 44, 42, 39, 35, 33, 29 ]
-    let result: number = 0;
-    return result;
+function safeOrUnsafe(report: number[], tolerance: number): number {
+    const increaseOrder: boolean = (report[0] - report[1]) < 0;
+    for (let i = 0; i < report.length - 1; i++) {
+        const diff: number = Math.abs(report[i] - report[i + 1]);
+        if (increaseOrder !== (report[i] - report[i + 1]) < 0
+            || (diff < 1 || diff > 3)) {
+            let valid: number = 0;
+            if (tolerance > 0) {
+                tolerance--;
+                valid = tryForValidReport(valid, report, i, tolerance);
+            }
+            return valid;
+        }
+    }
+    return 1;
 }
 
-console.log("Part 1:", part1(reports));
-console.log("Part 2:", part2(reports));
+function tryForValidReport(valid: number, report: number[], i: number, tolerance: number) {
+    valid = safeOrUnsafe([...report.slice(0, i - 1), ...report.slice(i, report.length)], tolerance);
+    if (valid == 0) {
+        valid = safeOrUnsafe([...report.slice(0, i), ...report.slice(i + 1, report.length)], tolerance);
+    }
+    if (valid == 0) {
+        valid = safeOrUnsafe([...report.slice(0, i + 1), ...report.slice(i + 2, report.length)], tolerance);
+    }
+    return valid;
+}
+
+console.log("Part 1:", solution(reports, 0));
+console.log("Part 2:", solution(reports, 1));
